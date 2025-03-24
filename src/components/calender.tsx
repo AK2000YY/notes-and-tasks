602c84921253bf62e222c0865ee44e3b558c774f
Abdulkarim <abdyabkor@gmail.com>
2025-03-24 05:15:07 +0300

    style calender component

--- src/components/calender.tsx
+++ src/components/calender.tsx
@@ -1,9 +1,51 @@
 import styles from '@/styles/style-body.module.css'
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md'
+
+const daysOfWeek: string[] = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY"
+];
+
+const monthsOfYear: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
 export function Calender() {
     return (
         <div className={styles.calender}>
-            <h1>Calender</h1>
+            <div className={styles.header}>
+                <h1>November, 2025</h1>
+                <div className={styles.buttons}>
+                    <MdOutlineKeyboardArrowLeft className={styles.button} />
+                    <MdOutlineKeyboardArrowRight className={styles.button} />
+                </div>
+            </div>
+            <div className={styles['days-name']}>
+                {daysOfWeek.map(ele => (
+                    <div key={ele}>{ele.substring(0, 3)}</div>
+                ))}
+            </div>
+            <div className={styles['days-number']}>
+                {Array.from({ length: 42 }).map((_, index) => (
+                    <div key={index}>{index + 1}</div>
+                ))}
+            </div>
         </div>
     )
 }
