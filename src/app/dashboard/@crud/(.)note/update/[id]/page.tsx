84b735f21cb74296c008b66bfebc7ce857c14cd5
Abdulkarim <abdyabkor@gmail.com>
2025-03-26 09:11:07 +0300

    your commit message

--- src/app/dashboard/@crud/(.)note/update/[id]/page.tsx
+++ src/app/dashboard/@crud/(.)note/update/[id]/page.tsx
@@ -1,7 +1,22 @@
-import { getNote } from "@/app/dashboard/note/update/[id]/page";
 import { NoteUpdate } from "@/components/note-update";
+import { db } from "@/db/drizzle";
+import { notesTable } from "@/db/schema/notes";
 import styles from '@/styles/style-body.module.css';
+import { eq } from "drizzle-orm";
+import { unstable_cache } from "next/cache";
 
+const getNote = unstable_cache(
+    async (noteId: number) => {
+        return await db
+            .select()
+            .from(notesTable)
+            .where(
+                eq(notesTable.id, noteId)
+            )
+    },
+    ['note'],
+    { revalidate: 3600, tags: ['note'] }
+)
 
 export default async function Page({ params }: {
     params: Promise<{ id: string }>
